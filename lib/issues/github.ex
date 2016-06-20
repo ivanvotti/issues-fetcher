5c49129f8748d82036583a5dd5ae3014394f75d1
Ivan Votti <ivanvotti@gmail.com>
2016-06-20 15:37:28 +0700

    Add logger to fetch_issues/2

--- lib/issues/github.ex
+++ lib/issues/github.ex
@@ -1,14 +1,20 @@
 defmodule Issues.Github do
+  require Logger
+
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch_issues(user, project) do
+    Logger.info("Fetching #{user}/#{project}...")
+
     issues_url(user, project)
     |> HTTPoison.get()
     |> case do
       {:ok, %{status_code: 200, body: body}} ->
+        Logger.info("Successful response")
         data = Poison.Parser.parse!(body)
         {:ok, data}
-      {_, %{body: body}} ->
+      {_, %{status_code: status_code, body: body}} ->
+        Logger.error("Error #{status_code} returned")
         reason = Poison.Parser.parse!(body)["message"]
         {:error, reason}
     end
